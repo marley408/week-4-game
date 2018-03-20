fd96c5e1a9f3eb10ab6c6eb21ed599191713a1bc
Matthew <mattsjsu14@gmail.com>
2018-03-19 21:23:00 -0700

    finished js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,88 @@
 
 let wins = 0
 let losses = 0
-// let randomNum;
-let jewel1= 3
-let jewel2= 8
-let jewel3= 10
-let jewel4 = 1
-
+let randomNum;
+let blueNum = 0;
+let greenNum = 0;
+let redNum = 0;
+let yellowNum = 0;
+let userScore;
 
 // set const variables for html elements that need updating on browser
-const winsElement = document.getElementById('wins')
-const lossesElement = document.getElementById('losses')
-const computerNumElement = document.getElementById('computerNum')
-const scoreElement = document.getElementById('score')
-
-
-
+const winsElement = $('#wins')
+const lossesElement = $('#losses')
+const computerNumElement = $('#computerNum')
+const scoreElement = $('#score')
 
 
 // The random number shown at the start of the game should be between 19 - 120.
 function setup(){
+    scoreElement.text(0);
+    randomNum = Math.floor(Math.random() * ((120-19)+1) + 19);
 
-    let randomNum = Math.floor(Math.random() * ((120-19)+1) + 19);
+    blueNum = randNum();
+    greenNum = randNum();
+    redNum = randNum();
+    yellowNum = randNum();
+    userScore = 0;
 
-    console.log(randomNum)
+    console.log({ 
+        randomNum: randomNum, 
+        blueNum: blueNum, 
+        greenNum: greenNum, 
+        redNum: redNum, 
+        yellowNum: yellowNum, 
+        userScore: userScore
+    });
 
-}
-
-
-function jewel1(){
-    let randomNum = Math.floor(Math.random() * 12 + 1);
-
-    console.log(randomNum)
-
-}
-
-function jewel2(){
-    let randomNum = Math.floor(Math.random() * 12 + 1);
-
-    console.log(randomNum)
-
-}
-
-function jewel3(){
-    let randomNum = Math.floor(Math.random() * 12 + 1);
-
-    console.log(randomNum)
+    computerNumElement.text(randomNum);
 
 }
 
-function jewel4(){
-    let randomNum = Math.floor(Math.random() * 12 + 1);
+setup()
 
-    console.log(randomNum)
+function checkScore(score){
+    if (score > randomNum){
+        losses++;
+        lossesElement.text(losses);
+        setup();
+    } else if (score === randomNum){
+        wins++;
+        winsElement.text(wins);
+        setup();
+    }
+}
 
+function handleScore(num) {
+    userScore = userScore + num;
+    scoreElement.text(userScore);
+    checkScore(userScore);
+}
+
+// here a number is picked for each crystal
+function randNum(){
+    return Math.floor(Math.random() * 12 + 1);
+}
+
+function handleBlueCrystalScore() {
+    handleScore(blueNum);
+}
+function handleGreenCrystalScore() {
+    handleScore(greenNum);
+}
+function handleRedCrystalScore() {
+    handleScore(redNum);
+}
+function handleYellowCrystalScore() {
+    handleScore(yellowNum);
 }
 
 
+// game function. player must use amount assigned to each crystal to match the random number. total score so far is added to score box. match = increase to win count. If userScore > randomNum, then increase to loss count. Restart after each win/loss
+
+$(document).ready(function() {
+    $('#crystal1').on('click', handleBlueCrystalScore);
+    $('#crystal2').on('click', handleGreenCrystalScore);
+    $('#crystal3').on('click', handleRedCrystalScore);
+    $('#crystal4').on('click', handleYellowCrystalScore);
+});
